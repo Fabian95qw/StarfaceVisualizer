166fedfd84e0734b84326869aa5eb9efa18b6479
Fabian95qw <fabian.zuend@bluewin.ch>
2018-07-30 16:51:51 +0200

    Callstate is now an enum instead of a string

--- Client/nucom/module/visualizer/call/CallStep.java
+++ Client/nucom/module/visualizer/call/CallStep.java
@@ -3,11 +3,14 @@ package nucom.module.visualizer.call;
 import java.util.List;
 
 import nucom.module.visualizer.utility.Log;
+import nucom.module.visualizer.utility.EnumHelper.CallState;
+import nucom.module.visualizer.utility.EnumHelper.TypeData;
 
 public class CallStep 
 {
-	private String CallState = "";
+	private CallState State = CallState.UNKNOWN;
 	private String DateTime ="";
+	private TypeData T= TypeData.UNKNOWN;
 	private List<Peer> Peers = null;
 	private Log log = null;
 	
@@ -16,10 +19,10 @@ public class CallStep
 		log = new Log(this.getClass());
 	}
 	
-	public void setCallState(String CallState)
+	public void setCallState(CallState State)
 	{
-		this.CallState=CallState;
-		log.debug("CallState: " + CallState);
+		this.State=State;
+		log.debug("CallState: " + State.toString());
 	}
 
 	public void setDateTime(String DateTime) 
@@ -34,8 +37,8 @@ public class CallStep
 		this.Peers=Peers;		
 	}
 
-	public String getCallState() {
-		return CallState;
+	public CallState getCallState() {
+		return State;
 	}
 
 	public String getDateTime() {
@@ -44,6 +47,16 @@ public class CallStep
 
 	public List<Peer> getPeers() {
 		return Peers;
+	}
+
+	public void setType(TypeData T) 
+	{
+		log.debug("Type: " +T.toString());
+		this.T=T;
+	}
+
+	public TypeData getT() {
+		return T;
 	}
 	
 	
